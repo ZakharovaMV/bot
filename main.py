dad423e3c3aac134ba7ab7a2ce509c6ead531111
mzv <zakharovamasha@mail.ru>
2023-03-06 00:17:21 +0300

    add

---
+++ main.py
@@ -0,0 +1,12 @@
+text = """ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
+nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate 
+"""
+
+# TODO здесь писать код
+import re
+
+text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate"
+print(re.findall(r"\b\w{4}\b", text))
+pass
